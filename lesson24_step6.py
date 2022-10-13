f5264995667a5644b6a7cdeb0229b02f9e7a0cc6
Аюпов Денис (Ayupov_DS) <Ayupov_DS@directum.ru>
2022-10-13 10:32:21 +0500

    Уроки stepik до 3-го блока

--- lesson24_step6.py
+++ lesson24_step6.py
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
+=======
+from selenium import webdriver
+>>>>>>> cc12ce9 (Уроки stepik до 3-го блока)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+<<<<<<< HEAD
+=======
+import time
+>>>>>>> cc12ce9 (Уроки stepik до 3-го блока)
 import math
 
 def calc(x):
@@ -12,15 +20,25 @@ browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
 price = WebDriverWait(browser, 5).until(
       EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+<<<<<<< HEAD
     )
+=======
+    )   
+>>>>>>> cc12ce9 (Уроки stepik до 3-го блока)
 print(price)
 button = browser.find_element(By.CSS_SELECTOR, "#book")
 button.click()
 
 # Выбор елемента x и вычисление капчи 
+<<<<<<< HEAD
 a_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
 a = int(a_element.text)
 y = calc(a)
+=======
+x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+x = int(x_element.text)
+y = calc(x)
+>>>>>>> cc12ce9 (Уроки stepik до 3-го блока)
 # Вывод решения капчи в поле ответа 
 input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
 input1.send_keys(y)
